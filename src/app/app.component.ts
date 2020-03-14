ca38ba2cdd303ecb2a8a085bd5ca004253abff6e
Boris <borisdralyuk@gmail.com>
2020-03-14 13:51:20 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'moonshot-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'creator-dashboard';
+}
